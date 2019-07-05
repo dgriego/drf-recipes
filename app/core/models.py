c1d7d307723ad054bc0f8ebf06940f7dd5188fd5
dgriego <eggomygriego@gmail.com>
2019-07-04 18:04:24 -0700

    add Recipe model and tests

--- app/core/models.py
+++ app/core/models.py
@@ -72,3 +72,22 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """
+    Recipe that conists of Ingredients and Tags
+    """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=50)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -81,3 +81,16 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """
+        Test the ingredient string representation
+        """
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='potato cakes',
+            time_minutes=30,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
